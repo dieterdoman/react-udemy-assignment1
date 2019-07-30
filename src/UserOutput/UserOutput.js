90bbb34e81fbeef8afa49a3f9ea2c07162a2a49c
Dieter Doman <dieter@jemstep.com>
2019-07-30 11:39:44 +0200

    added user input and output components.

---
+++ src/UserOutput/UserOutput.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const UserOutput = () => {
+    return (
+        <div>
+            <p>Paragraph1</p>
+            <p>Paragraph2</p>
+        </div>
+    );
+};
+
+export default UserOutput;
